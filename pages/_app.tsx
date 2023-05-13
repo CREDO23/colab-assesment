8baf3271045b384239f81915159f9f0fc5a2de14
CREDO23 <thierrybakera12@gmail.com>
2023-05-13 12:50:51 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import favicon from '../public/favicon.ico'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+  <Head>
+  <link rel="shortcut icon" href={favicon as unknown as string} />
+  </Head>
+     <Component {...pageProps} />
+  </>;
 }
